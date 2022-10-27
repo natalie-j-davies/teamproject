ff852d27f24f6673e1de70e45d1fc2fede46471f
Natalie Davies <210209859@aston.ac.uk>
2022-10-27 14:14:30 +0100

    created routes for home and about

--- g-twenty/routes/web.php
+++ g-twenty/routes/web.php
@@ -17,3 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', [HomeController::class, 'index']);
+
+Route::get('/about', [AboutController::class, 'index']);
