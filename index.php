615b699322f4b05e45c17ab322754e1f357fa8f0
Natalie Davies <116156662+natalie-j-davies@users.noreply.github.com>
2022-11-01 13:16:13 +0000

    Revert "Developer"

--- index.php
+++ index.php
@@ -4,9 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
+
+    <link rel = "stylesheet" type="text/css" href="style/style.css" />
+
+    <title>Test</title>
 </head>
 <body>
-    
+    <div id="nav-bar">
+        <a class="active" href="index.php">Home</a>
+        <a href="#products">Products</a>
+        <a href="#contact">Contact us</a>
+        <a href="#about">About</a>
+    </div>
+    <h1>Tester page to learn git</h1>
+    <p>Tester page for team project</p>
+
+    <h1>Welcome to Team Project !!!</h1>
+    <p>Tester page for uploading the code</p>
+    <p>Adding content to the index.html page</p>
+
+    <button id="button" onclick= "document.location='register.php'">Register here</button>
+
+    <h2>PHONES CASES</h2>
+    <h2>natalie test for pull change again test 2</h2>
 </body>
 </html>
