674e4a436aa9842b604d8b4775a1c7861bebbe5b
Natalie Davies <210209859@aston.ac.uk>
2022-11-07 16:19:53 +0000

    chnaged db connection, started log in

--- connectdb.php
+++ connectdb.php
@@ -1,12 +1,12 @@
 <?php
 
-$db_host = 'localhost:3306';
-$db_name = 'u_210209859_gtwentydb';
-$username = 'u-210209859';
-$password = 'qrygSvEBi5g3hga';
+$db_host = 'localhost';
+$db_name = 'gtwenty_db';
+$username = "root";
+$password = "";
    
 try {
-	$db = new PDO("mysql:dbname=$db_name;host=$db_host", $username, $password); 
+	$db = new PDO("mysql:host=$db_host;dbname=$db_name", $username, $password); 
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "";
 } catch(PDOException $ex) {
@@ -14,5 +14,5 @@ try {
 	echo($ex->getMessage());
 	exit;
 }
-?>
+
 ?>
--- index.php
+++ index.php
@@ -1,47 +1,3 @@
-<?php
-
-//if the form has been submitted
-if (isset($_POST['submitted'])){
-	if ( !isset($_POST['username'], $_POST['password']) ) {
-	// Could not get the data that should have been sent.
-	 exit('Please fill both the username and password fields!');
-	}
-	// connect DB
-	require_once ("connectdb.php");
-	try {
-	//Query DB to find the matching username + password
-	//using prepare to prevent SQL injection.
-		$stat = $db->prepare('SELECT password FROM users WHERE username = ?');
-		$stat->execute(array($_POST['username']));
-		
-		// fetch the result row and check 
-		if ($stat->rowCount()>0){ 
-			$row=$stat->fetch();
-
-			if (password_verify($_POST['password'], $row['userPassword'])){ 
-
-			  session_start();
-			  $_SESSION["username"]=$_POST['username'];
-			  header("Location:index.php");
-			  exit();
-			
-			} else {
-			 echo "<p style='color:red'>Error logging in, incorrect password </p>";
-			 }
-		} else {
-		  echo "<p style='color:red'>Error logging in, Username not found </p>";
-		}
-	}
-	catch(PDOException $ex) {
-		echo("Failed to connect to the database.<br>");
-		echo($ex->getMessage());
-		exit;
-	}
-	$username=$_SESSION['username'];
-	echo "<h2> Welcome ".$_SESSION['username']."! </h2>";
-	
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -65,14 +21,15 @@ if (isset($_POST['submitted'])){
 </div>
 <!--log in and clear button--> 
 <form action="index.php" method="post">
-<div id="index-login">
+<div id="login">
 	<label>User Name </label>
-	<input type="text" name="username" size="15" maxlength="25" />
+	<input type="text" name="username" value="" placeholder="Username"/>
+
 	<label>Password </label>
-	<input type="password" name="password" size="15" maxlength="25" />
-	<input type="submit" value="Login" class="button"/>
-	<input type="reset" value="clear" class="button"/>
-	<input type="hidden" name="submitted" value="TRUE" />
+	<input type="password" name="password" value="" placeholder="Password"/>
+
+	<input type="submit" value="Sign In" class="button"/>
+	<input type="hidden" name="login" value="TRUE" />
 	</form>
 
 </body>
