795cf3b194c79250381d98d2b739f2ae1aaa5400
Kazi Arham <210149902@aston.ac.uk>
2022-11-07 20:00:17 +0000

    Fixed the echo statement for copyright details

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
             <div class="footer">
 
             </div>
-            <small>&copy; <?php echo date('Y'); ?> <?php $companyName; ?></small>
+            <small>&copy; <?php echo date('Y'); ?> <?php echo $companyName; ?></small>
         </div>
         <!-- end content -->
     </div>
