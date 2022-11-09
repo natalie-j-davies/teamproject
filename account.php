96d9c705316c5dd5d2298373a45207ceb50bd7dc
Natalie Davies <210209859@aston.ac.uk>
2022-11-09 16:16:37 +0000

    added account page

---
+++ account.php
@@ -0,0 +1,14 @@
+<?php
+    define("TITLE", "Login | G-TWENTY");
+    include('includes/header.php');
+?>
+
+<h1>Account Information</h1>
+
+<h2>Details</h2>
+<h2>Recent Orders</h2>
+
+<p>Would you like to logout? <a href="logout.php">click here</p>
+<?php
+    include('includes/footer.php');
+?>
