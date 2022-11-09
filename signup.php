6567125595a3d6a4f869a7a82f017ab12604bef0
Kazi Arham <210149902@aston.ac.uk>
2022-11-09 16:04:27 +0000

    Added a field in the sign up form

--- signup.php
+++ signup.php
@@ -3,7 +3,7 @@
     include('includes/header.php');
 ?>
 
-            <!-- signup title -->
+            <!-- Signup title -->
             <div class="login-title text-center">
                 <h1 class="title">Sign Up</h1>
                 <p>Please fill in this sign-up form to create an account</p>
@@ -11,11 +11,14 @@
 
             <hr>
 
-            <!-- signup form -->
+            <!-- Sign-up form to sign up an account -->
             <div class="login-form">
                 <form action="#" id="signup-form">
                     <div class="container">
-                        
+                        <!-- Username Field -->
+                        <label for="username"><b>Username:</b></label>
+                        <input type="text" id="username" name="username"><br><br>
+
                     </div>
                 </form>
             </div>
