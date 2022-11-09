6138a84f0967616cda28e14f6a016c62d47307bb
Natalie Davies <210209859@aston.ac.uk>
2022-11-09 16:17:11 +0000

    deleted sign in req

--- index.php
+++ index.php
@@ -44,20 +44,9 @@ if (isset($_POST['login'])){
 
 }
 ?>
-  <!--log in and clear button--> 
-  <form action="index.php" method="post">
-    <div id="login">
-      <label>User Name </label>
-      <input type="text" name="username" value="" placeholder="Username"/>
 
-      <label>Password </label>
-      <input type="password" name="password" value="" placeholder="Password"/>
 
-      <input type="submit" value="Login" class="button"/>
-      <input type="hidden" name="login" value="TRUE" />
-    </div>
-  </form>
 
-  <?php
+<?php
     include('includes/footer.php');
 ?>
