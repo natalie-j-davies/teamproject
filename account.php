4d2801136e85fdc0fd19604d8972736a7a4b397b
Natalie Davies <210209859@aston.ac.uk>
2022-11-09 16:28:22 +0000

    added filler info

--- account.php
+++ account.php
@@ -3,12 +3,29 @@
     include('includes/header.php');
 ?>
 
-<h1>Account Information</h1>
 
-<h2>Details</h2>
-<h2>Recent Orders</h2>
+<div id="account-main">
+    <h1>Account Dashboard</h1>
+    <div id="account-details">
+        <h2>Details</h2>
+        <p>Name     </p>
+        <p>Email     </p>
+        <p>Username     </p>
+        <p>Password     </p>
 
-<p>Would you like to logout? <a href="logout.php">click here</p>
+    </div>
+    <div id="account-orders">
+        <h2>Recent Orders</h2>
+        <p>Product Name     </p>
+        <p>Order Number     </p>
+        <p>Order Date     </p>
+        <p>Shipping Address     </p>
+        <p>Total     </p>
+    </div>
+    <p>Would you like to logout? <a href="logout.php">click here</p>
+</div>
+
+
 <?php
     include('includes/footer.php');
 ?>
