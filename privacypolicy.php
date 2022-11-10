e498b9b9427771eb17f71329c1d07d43a2cfae6d
Natalie Davies <210209859@aston.ac.uk>
2022-11-10 13:48:47 +0000

    changed mixed up titles

--- privacypolicy.php
+++ privacypolicy.php
@@ -1,16 +1,23 @@
 <?php
-    define("TITLE", "Terms and Conditions| G-TWENTY");
+    define("TITLE", "Privacy Policy| G-TWENTY");
     include('includes/header.php');
 ?>
 <!--This is concept work not finished product -->
-<h1>Website Terms And Conditions</h1>
+<h1>PRIVACY POLICY</h1>
 <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores 
     et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id 
     est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi 
     optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
     Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae 
     non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut
-    perferendis doloribus asperiores repellat.</p>
+    perferendis doloribus asperiores repellat.<br>
+    At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et
+    quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est 
+    laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio 
+    cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus 
+    autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.
+    Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus 
+    asperiores repellat</p>
 <?php
     include('includes/footer.php');
 ?>
