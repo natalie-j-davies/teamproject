7c87b4eeafa6ce16a0afc734634e9b694e58a87e
Kazi Arham <210149902@aston.ac.uk>
2022-11-12 18:03:13 +0000

    Removed the colons for the field names

--- signup.php
+++ signup.php
@@ -16,35 +16,35 @@
                 <form action="signup.php" id="signup-form">
                     <div class="container">
                         <!-- Username Field -->
-                        <label for="username"><b>Username: </b></label>
+                        <label for="username"><b>Username </b></label>
                         <input type="text" id="username" name="username"><br><br>
 
                         <!-- Password Field -->
-                        <label for="password"><b>Password: </b></label>
+                        <label for="password"><b>Password </b></label>
                         <input type="password" id="password" name="password" required><br><br>
 
                         <!-- First Name Field -->
-                        <label for="firstname"><b>First Name: </b></label>
+                        <label for="firstname"><b>First Name </b></label>
                         <input type="text" id="firstname" name="firstname"><br><br>
 
                         <!-- Last Name Field -->
-                        <label for="lastname"><b>Last Name: </b></label>
+                        <label for="lastname"><b>Last Name </b></label>
                         <input type="text" id="lastname" name="lastname"><br><br>
                         
                         <!-- Phone Number Field -->
-                        <label for="phone"><b>Phone Number: </b></label>
+                        <label for="phone"><b>Phone Number </b></label>
                         <input type="text" id="phone" name="phone"><br><br>
 
                         <!-- Email Field -->
-                        <label for="email"><b>Email Address: </b></label>
+                        <label for="email"><b>Email Address </b></label>
                         <input type="text" id="email" name="email" required><br><br>
 
                         <!-- Address Line Field -->
-                        <label for="address"><b>Address Line: </b></label>
+                        <label for="address"><b>Address Line </b></label>
                         <input type="text" id="address" name="address"><br><br>
 
                         <!-- Postcode Field -->
-                        <label for="postcode"><b>Postcode: </b></label>
+                        <label for="postcode"><b>Postcode </b></label>
                         <input type="text" id="postcode" name="postcode"><br><br>
 
                         <p>By creating an account you agree to G-TWENTY's Terms and Conditions. Please see our <a href="privacypolicy.php">Privacy Policy</a>.</p>
