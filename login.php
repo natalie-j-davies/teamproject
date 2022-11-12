ed8012c5c582514ae51c8be959fcd9bfae6ab588
Kazi Arham <210149902@aston.ac.uk>
2022-11-12 18:13:19 +0000

    Added div tags for the login page

--- login.php
+++ login.php
@@ -3,23 +3,30 @@
     include('includes/header.php');
 ?>
 
-<p>Sign in here..</p>
+  <!-- Login title -->
+  <div class="login-title text-center">
+    <h1 class="title">Login</h1>
+    <p>Please sign in here to login</p>
+  </div>
+
   <!--log in and clear button--> 
-  <form action="account.php" method="post">
-    <div id="login">
-      <label>User Name </label>
-      <input type="text" name="username" value="" placeholder="Username"/>
+  <div class="login-form">
+    <form action="account.php" method="post">
+      <div class="container" id="login">
+        <label>User Name </label>
+        <input type="text" name="username" value="" placeholder="Username"/>
 
-      <label>Password </label>
-      <input type="password" name="password" value="" placeholder="Password"/>
+        <label>Password </label>
+        <input type="password" name="password" value="" placeholder="Password"/>
 
-      <input type="submit" value="Login" class="button"/>
-      <input type="hidden" name="login" value="TRUE" />
-    </div>
-  </form>
+        <input type="submit" value="Login" class="button"/>
+        <input type="hidden" name="login" value="TRUE" />
+      </div>
+    </form>
+  </div>
 
   <p>Not a member? Register <a href="signup.php">here</a></p>
-  <p>By signing-in you agree to G-TWENTY's Terms and Conditions. Please see our <a href="privacypolicy.php">Privacy Policy.</a></p>
+  <p>By signing-in you agree to G-TWENTY's Terms and Conditions. Please see our <a href="privacypolicy.php">Privacy Policy</a>.</p>
 <?php
     include('includes/footer.php');
 ?>
