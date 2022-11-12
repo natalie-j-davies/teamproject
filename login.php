12a18c518f385a0b14a3777b98562b29a4820779
Kazi Arham <210149902@aston.ac.uk>
2022-11-12 18:22:27 +0000

    Fixed bugs in the login page

--- login.php
+++ login.php
@@ -9,24 +9,33 @@
     <p>Please sign in here to login</p>
   </div>
 
-  <!--log in and clear button--> 
+  <hr>
+
+  <!-- Log in form and button --> 
   <div class="login-form">
     <form action="account.php" method="post">
       <div class="container" id="login">
-        <label>User Name </label>
+        <!-- Username Field -->
+        <label for="username">Username </label>
         <input type="text" name="username" value="" placeholder="Username"/>
 
-        <label>Password </label>
+        <!-- Password Field -->
+        <label for="password">Password </label>
         <input type="password" name="password" value="" placeholder="Password"/>
 
+        <!-- Login Button -->
         <input type="submit" value="Login" class="button"/>
         <input type="hidden" name="login" value="TRUE" />
+
+        <div class="signupLink clearfix">
+          <p>Not a member? Register <a href="signup.php">here</a></p>
+        </div>
+
+        <p>By signing-in you agree to G-TWENTY's Terms and Conditions. Please see our <a href="privacypolicy.php">Privacy Policy</a>.</p>
       </div>
     </form>
   </div>
 
-  <p>Not a member? Register <a href="signup.php">here</a></p>
-  <p>By signing-in you agree to G-TWENTY's Terms and Conditions. Please see our <a href="privacypolicy.php">Privacy Policy</a>.</p>
 <?php
     include('includes/footer.php');
 ?>
