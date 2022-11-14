d35171165c056fb5a54c894c0a19df2b8b27daa7
Mohammed Ali <210132948@aston.ac.uk>
2022-11-14 19:31:58 +0000

    edited two pages

--- about.php
+++ about.php
@@ -3,6 +3,24 @@
     include('includes/header.php');
 ?>
 
+    
+        
+        <section class="about">
+         <div class="main">
+            <img src="twenty.jpg">
+            <div class="about-text">
+                <h1>About Us</h1>
+                <h5>About<span> G-Twenty</span></h5>
+                <br>
+                <p>G-Twenty was founded in 2022 bya group of Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati sequi quaerat reiciendis numquam suscipit quos voluptas cum velit. Corporis perferendis itaque harum rem autem sint hic! Nam quae nobis alias?</p>
+                <br>
+                <button type="button">View Our Latest Products?</button>
+            </div>
+    
+
+         </div>   
+        </section>
+
 
 <?php
     include('includes/footer.php');
