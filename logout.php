e2a6f07c408b25818c608e84c9bade116c47b469
Natalie Davies <210209859@aston.ac.uk>
2022-11-15 11:15:11 +0000

    logout bug

--- logout.php
+++ logout.php
@@ -1,12 +1,7 @@
 <?php
-	session_start();
-
-	session_destroy();
-
-?>
-<?php
     define("TITLE", "Log Out | G-TWENTY");
     include('includes/header.php');
+	session_destroy();
 ?>
 
 <div id="log-out">
