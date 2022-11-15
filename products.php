fcbffc6ae841293488bb80828cc27c2266c66344
Daanyaal Chughtai <210049853@aston.ac.uk>
2022-11-15 15:40:49 +0000

    added rough search bar

--- products.php
+++ products.php
@@ -2,8 +2,18 @@
     define("TITLE", "Products | G-TWENTY");
     include('includes/header.php');
 ?>
+<div class="titleList">
+<h1>Products</h1>
+</div>
+<div class="searchform">
+    <form>
+        <div class="searchbar text-center">
+        <input type class="form-control" name="search" type="search" placeholder="Search Phone Cases">
+       
+    </form>
 
-
+</div>
+<div class ="sortby">Sort by:</div>
 
 <?php
     include('includes/footer.php');
