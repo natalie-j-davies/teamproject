607ee090a13e2dab71e2bbf79aecc5d28ab52740
Daanyaal Chughtai <210049853@aston.ac.uk>
2022-11-16 09:37:28 +0000

    Edit products page

--- products.php
+++ products.php
@@ -3,6 +3,26 @@
     include('includes/header.php');
 ?>
 
+<div class="titleList">
+<h1>Products</h1>
+</div>
+<div class="searchform">
+    <form>
+        <div class="searchbar text-center">
+        <input type class="form-control" name="search" type="search" placeholder="Search Phone Cases">
+       
+    </form>
+
+</div>
+<br>
+<div class ="sortby">
+ <button class="sortbybutton">Sort By</button>
+    
+ </div>
+
+<div class ="filter">
+    <button class="filterbutton">Filter</button>
+</div>
 
 
 <hr>
