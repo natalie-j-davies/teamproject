a29dd2831e41ce743e65e230adc767ce84ca0bdd
Natalie Davies <210209859@aston.ac.uk>
2022-11-16 16:41:21 +0000

    deleted extra css link

--- includes/header.php
+++ includes/header.php
@@ -16,7 +16,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="style/style.css" />
-    <link rel="stylesheet"  type="text/css" href="style/AboutUs.css">
   <!--<link rel="stylesheet"  type="text/css" href="style/AboutUs.css">
     <link rel="stylesheet"  type="text/css" href="style/ContactUs.css">-->
     <!-- Bootstrap -->
