ffdfc41d315d53cc6025436615c9c54a44d83d72
Kazi Arham <210149902@aston.ac.uk>
2022-11-16 11:23:03 +0000

    Edited

--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,17 @@
             <div class="footer">
                 <div class="column three">
                     <!-- Social media -->
+
                 </div>
 
                 <div class="column three">
                     <!-- Privacy Policy -->
+
                 </div>
 
                 <div class="column three last">
                     <!-- FAQ -->
+                    
                 </div>
             </div>
             <small>Copyright&copy; <?php echo date('Y'); ?> <?php echo $companyName; ?>
