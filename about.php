8289ac9be575b1fdc134cade1bd001f967699983
AsyaAlly <210099995@aston.ac.uk>
2022-11-16 11:02:15 +0000

    newchanges

--- about.php
+++ about.php
@@ -8,7 +8,7 @@
             <img src="twenty.jpg">
             <div class="about-text">
                 <h1>About Us</h1>
-                <h5>About<span> G-Twenty</span></h5>
+                <h5>About<span class="test" > G-Twenty</span></h5>
                 <br>
                 <p>G-Twenty was founded in 2022 bya group of Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati sequi quaerat reiciendis numquam suscipit quos voluptas cum velit. Corporis perferendis itaque harum rem autem sint hic! Nam quae nobis alias?</p>
                 <br>
