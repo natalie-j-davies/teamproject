c0d28940626b6d86ddad7ce610e8868990db599f
Kazi Arham <210149902@aston.ac.uk>
2022-11-17 11:20:35 +0000

    Added content for the Get Help column in footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,19 +1,24 @@
             <div class="footer">
-                <div class="column three">
+                <div class="column three col-sm-3">
                     <!-- Social media -->
 
                 </div>
 
-                <div class="column three">
+                <div class="column three col-sm-3">
                     <!-- Privacy Policy -->
                     <img src="images/logo/logo.png" alt="img" id="temp-logo"></img>    
                     <p><a href='privacypolicy.php'>Privacy Policy</a><br>
                     <a href='termsconditions.php'>Term and Conditions</a></p>
                 </div>
 
-                <div class="column three last">
+                <div class="column three last col-sm-3">
                     <!-- FAQ -->
-                    
+                    <h3>Get Help</h3>
+                    <ul>
+                        <li>FAQs</li>
+                        <li><a href="about.php">Who we are?</a></li>
+                        <li><a href="contact.php">Contact Us</a></li>
+                    </ul>
                 </div>
                 <small>Copyright&copy; <?php echo date('Y'); ?> <?php echo $companyName; ?></small>
             </div>
