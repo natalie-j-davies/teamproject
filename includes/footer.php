4fd87083b82cd0682fc306834a095388c1837936
Kazi Arham <210149902@aston.ac.uk>
2022-11-17 11:08:37 +0000

    Fixed the indentation

--- includes/footer.php
+++ includes/footer.php
@@ -6,9 +6,9 @@
 
                 <div class="column three">
                     <!-- Privacy Policy -->
-                <img src="images/logo/logo.png" alt="img" id="temp-logo"></img>    
-                <p><a href='privacypolicy.php'>Privacy Policy</a><br>
-                <a href='termsconditions.php'>Term and Conditions</a></p>
+                    <img src="images/logo/logo.png" alt="img" id="temp-logo"></img>    
+                    <p><a href='privacypolicy.php'>Privacy Policy</a><br>
+                    <a href='termsconditions.php'>Term and Conditions</a></p>
                 </div>
 
                 <div class="column three last">
