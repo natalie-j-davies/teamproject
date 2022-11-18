55ca03bf535ec49f2d6f7b6aec426d0dcd62faeb
Natalie Davies <210209859@aston.ac.uk>
2022-11-18 11:25:03 +0000

    account-info pull

--- account.php
+++ account.php
@@ -3,61 +3,60 @@
     include('includes/header.php');
 
 ?>
-	<?php
+<?php
 	//start the session, check if the user is not logged in, redirect to start
-	if (!isset($_SESSION['username'])){
+    if (!isset($_SESSION['username'])){
 		header("Location: login.php");
 		exit();
 	}
-	$username=$_SESSION['username'];
 
 	?>
 <!--The Account dashboard will show all user information when user logs in -->
-    <div id="account-container">
+<div id="account-container">
+<div id='acccount-info-box'>
 <?php
-	include ('connectdb.php'); 
-    $query = "SELECT * FROM users WHERE username = '$username'";
-    $rows =  $db->query($query);
+	$username=$_SESSION['username'];
 
-    echo "<h2>".$_SESSION['username']."'s Account Dashboard</h2>";
+/* Prints the username of current logged in user */
+    echo "<h2>". $username ."'s Account Dashboard</h2>";
+	include ('connectdb.php'); 
+
+try{
+    $userid = $_SESSION['username'];
+    $query = "SELECT * FROM users WHERE username = '$userid'";
+    $rows =  $db->query($query);
     
-    if ( $rows && $rows->rowCount()> 0) {
+
+    if ($rows && $rows->rowCount()> 0) {
                 while  ($row =  $rows->fetch())	{
-                    echo "<div id='acccount-info-box'>
+                    echo "
                     <h3>Profile</h3>
                     <p>Name:  ". $row['firstName'] ." ". $row['lastName'] ."</p>
                     <p>Email: ". $row['email'] ."</p>
-                    <p>Username ". $row['username'] ."</p>
+                    <p>Username: ". $row['username'] ."</p>
                     <p>Address: ". $row['addressLine'] ."</p>
-                    <p>Postcode: <br>". $row['postcode'] ."</p>
+                    <p>Postcode: ". $row['postcode'] ."</p>
                     </div>";
 
                 }
-            }else{
-                echo "<h3 style= 'color:red'>System Error</h3>";
             }
-/*
-        <div id="account-orders">
-            <h2>Recent Orders</h2>
-            <p>Product Name     </p>
-            <p>Order Number     </p>
-            <p>Order Date     </p>
-            <p>Shipping Address     </p>
-            <p>Total     </p>
-        </div>*/
-
+            else {
+                echo "<div id='acccount-info-box'>
+                <h3>Profile</h3>
+                <p>Name:  Database Error</p>
+                <p>Email:   Database Error</p>
+                <p>Username:   Database Error</p>
+                <p>Address:   Database Error</p>
+                <p>Postcode:   Database Error</p>
+                </div>";
+            }
+        }
+catch (PDOexception $ex){
+    echo "Sorry, a database error occurred! <br>";
+    echo "Error details: <em>". $ex->getMessage()."</em>";
+}
 ?>
 
-<div id="account-main">
-    <h1>Account Dashboard</h1>
-    <div id="account-details">
-        <h2>Details</h2>
-        <p>Name     </p>
-        <p>Email     </p>
-        <p>Username     </p>
-        <p>Password     </p>
-
-    </div>
     <div id="account-orders">
         <h2>Recent Orders</h2>
         <p>Product Name     </p>
