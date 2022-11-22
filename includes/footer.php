a9d3826cabc4a8d0b92b08e767cf69d6502d3127
Kazi Arham <210149902@aston.ac.uk>
2022-11-22 19:02:06 +0000

    Adding CSS to the copyright section

--- includes/footer.php
+++ includes/footer.php
@@ -3,9 +3,10 @@
                 <div class="column three">
                     <!-- Social media -->
                     <h4 class="footer-heading">Social Media</h4>
+                    <br>
                     <ul id="social-media-info">
-                        <li>Instagram: G-Twenty2022</li>
-                        <li>Twitter: G--Twenty2022</li>
+                        <li>Instagram: G-Twenty2022</li><br>
+                        <li>Twitter: G--Twenty2022</li><br>
                         <li>Facebook: G-Twenty2022</li>
                     </ul>
                 </div>
@@ -13,18 +14,19 @@
                 <div class="column three">
                     <!-- Privacy Policy -->
                     <h4 class="footer-heading"><img src="images/logo/logo.png" alt="img" id="footer-logo"></img> Policies</h4>
-                     
+                    <br>
                     <ul>
-                        <li><p><a href='privacypolicy.php'>Privacy Policy</a><br></li>
-                        <li><a href='termsconditions.php'>Term and Conditions</a></p></li>
+                        <li><a href='privacypolicy.php'>Privacy Policy</a></li><br>
+                        <li><a href='termsconditions.php'>Term and Conditions</a></li>
                     </ul>
                 </div>
 
                 <div class="column three last">
                     <!-- FAQ -->
                     <h4 class="footer-heading">Get Help</h4>
+                    <br>
                     <ul class="get-help-info">
-                        <li><a href="about.php">Who we are?</a></li>
+                        <li><a href="about.php">Who we are?</a></li><br>
                         <li><a href="contact.php">Contact Us</a></li>
                     </ul>
                 </div>
