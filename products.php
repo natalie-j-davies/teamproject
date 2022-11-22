50bf64cfd3115f6db1f59a75756c8318dfc02dad
Daanyaal Chughtai <210049853@aston.ac.uk>
2022-11-22 13:52:20 +0000

    changed buttons & added sidebar

--- products.php
+++ products.php
@@ -8,14 +8,15 @@
 </div>
 <div class="searchform">
     <form>
-        <div class="searchbar text-center">
+        <div class="searchbar">
         <input type class="form-control" name="search" type="search" placeholder="Search Phone Cases">
        
     </form>
-
+    
 </div>
 <br>
-<div class = "productsection">
+<div class = prodbuttonsection>
+<div class = "prodbutton">
  <button class="productbutton sortbybutton">Sort By</button>
  <div class ="productdropdown">
     <a href="#">Alphabetically</a>
@@ -24,28 +25,57 @@
     
  </div>
 
-<div class ="productsection">
-    <button class="productbutton materialbutton">Case Material</button>
+<div class ="prodbutton">
+    <button class="productbutton materialbutton">Price</button>
     <div class ="productdropdown">
-    <a href="#">Clear Cases</a>
-    <a href="#">Leather Cases</a>
-    <a href="#">Metal Cases</a>
-    <a href="#">Wood Cases</a>
-    </div>  
- </div>
- <div class ="productsection">
-    <button class="productbutton stylebutton">Case Style</button>
-    <div class ="productdropdown">
-    <a href="#">Cartoon</a>
-    <a href="#">Futuristic</a>
-    <a href="#">Simple</a>
+    <a href="#">Under £30</a>
+    <a href="#">£30 - £40</a>
+    <a href="#">£50 - £60</a>
+    <a href="#">Over £60</a>
     </div>  
  </div>
  
+</div>
 
 
 
+<br>
+<br>
 <hr>
+<div class="sidebar">
+    <div class = "sidebarcategories">Case Style</div>
+    <label class="container">Cartoon
+    <input type="checkbox">
+    <span class="checkmark"></span>
+    </label>
+    <label class="container">Futuristic
+    <input type="checkbox">
+    <span class="checkmark"></span>
+    </label>
+    <label class="container">Simple
+    <input type="checkbox">
+    <span class="checkmark"></span>
+    </label>
+    <hr>
+    <div class = "sidebarcategories">Case Material</div>
+    <label class="container">Clear Cases
+    <input type="checkbox">
+    <span class="checkmark"></span>
+    </label>
+    <label class="container">Leather Cases
+    <input type="checkbox">
+    <span class="checkmark"></span>
+    </label>
+    <label class="container">Metal Cases
+    <input type="checkbox">
+    <span class="checkmark"></span>
+    </label>
+    <label class="container">Wood Cases
+    <input type="checkbox">
+    <span class="checkmark"></span>
+    </label>
+   
+</div>
 <?php
     include('includes/footer.php');
 ?>
