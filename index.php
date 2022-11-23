71398c722966044fb9fc778c64477b007385cb59
Sydul Bari <210180190@aston.ac.uk>
2022-11-23 10:59:09 +0000

    images alignment

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
 <!--feature collection with links to products --> 
 <h4>Featured Collection</h4>
 <div id="middle-container">
-  <div><img id="pic-2" src="images/temp_images/clearcase.JPEG"><p id="FC-label">Clear Cases</p></div>
-  <div><img id="pic-3" src="images/temp_images/leathercase.JPEG"><p id="FC-label">Leather Cases</p></div>
-  <div><img id="pic-4" src="images/temp_images/metalcase.JPEG"><p id="FC-label">Metal Cases</p></div>
-  <div><img id="pic-5" src="images/temp_images/woodcase.JPEG"><p id="FC-label">Wood Cases</p></div>
+  <div id ="upper-padding"><img id="pic-2" src="images/temp_images/clearcase.JPEG"><p id="FC-label">Clear Cases</p></div>
+  <div id ="upper-padding"><img id="pic-3" src="images/temp_images/leathercase.JPEG"><p id="FC-label">Leather Cases</p></div>
+  <div id ="upper-padding"><img id="pic-4" src="images/temp_images/metalcase.JPEG"><p id="FC-label">Metal Cases</p></div>
+  <div id ="upper-padding"><img id="pic-5" src="images/temp_images/woodcase.JPEG"><p id="FC-label">Wood Cases</p></div>
 </div>
 <!--our community section - more pictures of phone cases/people to inspire user to shop--> 
   <h4>Newest Designs</h4>
@@ -24,6 +24,7 @@
     <div id="lower-padding"><img id="pic-6" src="images/cases/brasspeaks.png"><p id="FC-label">Brass Peaks</p></div>
     <div id="lower-padding"><img id="pic-7" src="images/cases/obsidian_14proMax.png"><p id="FC-label">Obsidian</p></div>
     <div id="lower-padding"><img id="pic-8" src="images/cases/snowyPeaks_GPxlPro.png"><p id="FC-label">Snowy Peaks</p></div>
+    <div id="lower-padding"><img id="pic-9" src="images/cases/hua30.png"><p id="FC-label">Refraction</p></div>
   </div>
 
 <?php
