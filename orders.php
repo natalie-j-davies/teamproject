e07b83942ec800eff8fd571b7753861c07dbc6ae
Natalie Davies <210209859@aston.ac.uk>
2022-11-24 15:27:46 +0000

    order-admin page

--- orders.php
+++ orders.php
@@ -4,10 +4,53 @@
 
 ?>
 
-<div id='customer-header'>
+<div id='orders-header'>
 <h1 id='account-title'><img src='images/logo/logo-symbol.png' alt='img' id='account-logo'>G-Twenty Orders</h1>
 </div>
 
+<div id="orders-container">
+<?php
+
+include ('connectdb.php'); 
+    try{
+        $query = "SELECT * FROM orders;";
+        $rows =  $db->query($query);
+            if ($rows && $rows->rowCount()> 0) {
+                echo " 
+                    <table id='orders-table'>
+                    <tr>
+                    <th>User Account ID</th>
+                    <th>Order Number </th>
+                    <th>Order Date </th>
+                    <th>Total Price </th>
+
+                    </tr>";            
+                        while  ($row =  $rows->fetch())	{
+                            
+                        echo "<tr>
+                            <td>". $row['userAccountID'] ."</td>
+                            <td>". $row['orderID'] ."</td>
+                            <td>". $row['created_at'] ."</td>
+                            <td>£". $row['total_price'] ."</td>
+                        </tr>
+                    ";
+
+            }
+        }
+            else {
+                    echo "<h2>Recent Orders</h2>
+                            <p> You have no recent orders</p>";
+            }
+
+    }
+    catch (PDOexception $ex){
+        echo "Sorry, a database error occurred! <br>";
+        echo "Error details: <em>". $ex->getMessage()."</em>";
+    }
+?>
+        </table>
+    </div>
+ </div>
 <?php
     include('includes/footer.php');
 ?>
