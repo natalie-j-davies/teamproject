15f30714842c75bc6fa2b827d592dd674d478591
Natalie Davies <210209859@aston.ac.uk>
2022-11-27 16:38:54 +0000

    search bar added and fixed

--- customers.php
+++ customers.php
@@ -6,18 +6,17 @@
 <div id='customer-header'>
 <h1 id='account-title'><img src='images/logo/logo-symbol.png' alt='img' id='account-logo'>G-Twenty Customer List</h1>
 </div>
-
-<?php
-/* Prints the information of all customers */
-
-include ('connectdb.php'); 
-
-try{
-$query = "SELECT * FROM users";
-$rows =  $db->query($query);
-
-if ($rows && $rows->rowCount()> 0) {
-    echo "<table id='customer-table'>
+<div id="searchbar-stock">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" class="form-inline">
+        <div class="form-group">
+            <div class="input-group">
+                <input type class="form-control" name="search" type="search" placeholder="Search key words..">
+                <span class="input-group-btn"><button class="btn btn-secondary" name="submit" type="submit"><i class="fa fa-search"></i></button></span>
+            </div>
+        </div>
+    </form>
+</div>
+<table id='customer-table'>
             <tr>
                 <th>Username</th>
                 <th>Full Name</th>
@@ -25,27 +24,57 @@ if ($rows && $rows->rowCount()> 0) {
                 <th>Phone</th>
                 <th>Address Line</th>
                 <th>Postcode</th>
-            </tr>";
-            while  ($row =  $rows->fetch())	{
-                echo "<tr> <td>". $row['username'] ."</td>";
-                echo "<td>". $row['firstName'] ." ". $row['lastName'] ."</td>";
-                echo "<td>". $row['email'] ."</td>";
-                echo "<td>". $row['phone'] ."</td>";
-                echo "<td>". $row['addressLine'] ."</td>";
-                echo "<td>". $row['postcode'];
-                echo "</td> </tr>";
+                </tr>
+<?php
 
+include ('connectdb.php');  
+
+try {
+    if(!isset($_POST['submit'])){
+        $query="SELECT  * FROM  users";
+        $rows =  $db->query($query);	
+        if ( $rows && $rows->rowCount()> 0) {
+            while  ($row =  $rows->fetch())	{  
+                echo "<tr> 
+                <td>". $row['username'] ."</td>
+                <td>". $row['firstName'] ." ". $row['lastName'] ."</td>
+                <td>". $row['email'] ."</td>
+                <td>". $row['phone'] ."</td>
+                <td>". $row['addressLine'] ."</td>
+                <td>". $row['postcode']. "</td>";
             }
         }
-        else {
-            echo "
-            <h3>Database ERROR please get in touch with IT on 01274388838</h3>";
-        }
     }
-catch (PDOexception $ex){
-echo "Sorry, a database error occurred! <br>";
-echo "Error details: <em>". $ex->getMessage()."</em>";
+    if(isset($_POST['submit'])){
+        if(!empty($_POST['search'])){
+            $search= $_POST['search'];
+
+            $query="SELECT  * FROM  users WHERE userAccountId LIKE '%$search%' OR username LIKE '%$search%' OR firstName LIKE '%$search%' OR lastName LIKE '%$search%'
+            OR phone LIKE '%$search%' OR email LIKE '%$search%' OR addressLine LIKE '%$search%' OR postcode LIKE '%$search%';";
+            $rows =  $db->query($query);
+
+                if ( $rows && $rows->rowCount()> 0) {
+                    while  ($row =  $rows->fetch())	{ 
+                        echo "<tr> 
+                        <td>". $row['username'] ."</td>
+                        <td>". $row['firstName'] ." ". $row['lastName'] ."</td>
+                        <td>". $row['email'] ."</td>
+                        <td>". $row['phone'] ."</td>
+                        <td>". $row['addressLine'] ."</td>
+                        <td>". $row['postcode']. "</td>";
+                    }
+                }
+            }else if(!empty($_POST['search'])){
+                echo  "<p>0 results for '$search'.</p>\n"; //no match found
+            }
+    }
+    
 }
+catch (PDOexception $ex){
+    echo "Sorry, a database error occurred! <br>";
+    echo "Error details: <em>". $ex->getMessage()."</em>";
+}
+
 ?>
 </table>
 </div>
