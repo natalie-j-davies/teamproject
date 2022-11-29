fd117a6cfd3a4fa6242422afc55c5cb2d18b8150
Natalie Davies <210209859@aston.ac.uk>
2022-11-29 17:55:12 +0000

    log out CSS

--- logout.php
+++ logout.php
@@ -8,13 +8,13 @@
     define("TITLE", "Log Out | G-TWENTY");
     include('includes/header.php');
 ?>
-
+<div id="log-out-container">
 <div id="log-out">
 	<h2> Logged out of your G-TWENTY account </h2> 
 	<p>Would like to log in again? <br></p>
-	<button id="button" onclick= "document.location='login.php'">Log in</button>
+	<button class="about-button" onclick= "document.location='login.php'">Log in</button>
 </div>
-
+</div>
 <?php
     include('includes/footer.php');
 ?>
