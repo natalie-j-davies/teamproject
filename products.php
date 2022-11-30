13f3a242f06b145e4c8904167f2dbe887249a195
Natalie Davies <210209859@aston.ac.uk>
2022-11-30 16:43:53 +0000

    button fix

--- products.php
+++ products.php
@@ -288,7 +288,7 @@
 				<h4>Price: £". $row['price'] ."</h4>
                 <p>Brand: ". $row['caseBrand'] ."</p>
                 <p>Colour: ". $row['caseColour'] ."</p>
-                <button class='basket-button' type='submit'><a href='view.php?SKU=". $row['SKU']. "'>View Product</a></button>
+                <a href='view.php?SKU=". $row['SKU']. "'><button class='basket-button' type='submit'>View Product</button></a>
                 </div>
 				</div>";
 
