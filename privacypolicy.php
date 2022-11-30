312820af92f2bef56bb3d9a4c8d565162a496c81
Natalie Davies <210209859@aston.ac.uk>
2022-11-30 12:04:15 +0000

    ASYA's text +css changes

--- privacypolicy.php
+++ privacypolicy.php
@@ -3,14 +3,36 @@
     include('includes/header.php');
 ?>
 <!--This is concept work not finished product -->
-<h1>PRIVACY POLICY</h1>
-<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores 
-    et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id 
-    est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi 
-    optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
-    Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae 
-    non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut
-    perferendis doloribus asperiores repellat.<br>
+<h1 class="pp-header"><img class="contact-logo"src="images/logo/logo-symbol.png">PRIVACY POLICY</h1>
+<div class="pp-container">
+<p>At G-TWENTY limited we value the importance of keeping your personal data safe. 
+    This Privacy Policy ("Policy") describes how G-TWENTY collects, processes, uses, disseminates 
+    or makes available your personal data via the G-TWENTY website, including G-TWENTY mobile applications
+    (collectively, "Website") and other means (i.e, social media, cookies, etc.).
+.<br>
+<div class="pp-upper">
+<b>Last Modified Date: 23 November 2022</b><br>
+<b>What Is Personal Data?</b>
+For the purpose of this Policy, Personal Data means and includes your name, phone number, address, 
+email address, online identifiers and any other data which, directly or indirectly, can identify you.
+</div>
+<br>
+<div class="pp-lower">
+Personal Data We Collect:
+Information Voluntarily Provided To Us. We collect your Personal Data and other information when you 
+visit our Website, place orders, make purchases, sign up or create an account with us, when you interact 
+with our customer service for support or giving us a feedback, when you visits our retail/pop-up stores, 
+or when you subscriber/participate in our survey/review or marketing activities 
+(e.g., subscribing to our newsletter/mailing list, participating in our loyalty programs, contests and sweepstakes, etc.). 
+
+The Personal Data obtained and collected by us includes:
+Contact information (i.e, your name, phone number, postal address, email address, birthday and any other personal data)
+Details of purchase/order your made and your purchase history
+Your payment information, such as billing address, credit card information and other information required for placing 
+order or to complete the purchase
+
+Your consumer behaviours and habits
+Information and reason of contacting our customer service, and communication content between you and our customer service
     At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et
     quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est 
     laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio 
@@ -18,6 +40,8 @@
     autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.
     Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus 
     asperiores repellat</p>
+</div>
+</div>
 <?php
     include('includes/footer.php');
 ?>
